267f7c6c9cc81aa3dfc5828c8794baa85bc2ae49
John Adrian Bonto <bontojohnadrian@gmail.com>
2024-06-21 21:19:01 +0800

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,45 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import "../data/backend-practice.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      // make it go to the next step (then)
+      // resolve() = let us control the next step
+      resolve("val1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     // make it go to the next step (then)
+//     // resolve() = let us control the next step
+//     resolve("val1");
+//   });
+// })
+//   .then((value) => {
+//     console.log(value);
+//     return new Promise((resolve) => {
+//       loadCart(() => {
+//         resolve();
+//       });
+//     });
+//   })
+
+//   .then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+
+// use promise instead of callbacks to prevent many nesting and make it hard to manage
